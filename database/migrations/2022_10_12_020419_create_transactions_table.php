495e73027ee5904d2e856500aac316f646039cd6
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-10-12 09:09:48 +0700

    create model and migration for transaction tabel

---
+++ database/migrations/2022_10_12_020419_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('appointment_id');
+            $table->varchar('fee_doctor')->nullable();
+            $table->varchar('fee_specialist')->nullable();
+            $table->varchar('fee_hospital')->nullable();
+            $table->varchar('sub_total')->nullable();
+            $table->varchar('vat')->nullable();
+            $table->varchar('total')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
