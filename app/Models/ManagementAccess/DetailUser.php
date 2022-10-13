8c11e3c24a8f5c882718b30f77c282fd7572a83e
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-10-13 20:05:02 +0700

    create detail user model configuration

--- app/Models/ManagementAccess/DetailUser.php
+++ app/Models/ManagementAccess/DetailUser.php
@@ -2,10 +2,20 @@
 
 namespace App\Models\ManagementAccess;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DetailUser extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    use SoftDeletes;
+
+    public $table = 'detail_users';  // point to table name
+
+    protected $dates = [  // to save date format
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
 }
